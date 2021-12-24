83c64913bb9f2cf5a8d3e141140a326db0e29644
H0nok4 <45478238+chenweidada@users.noreply.github.com>
2021-12-25 01:04:00 +0800

    assets:暂时用不上Mono的单例

--- Project-M/Assets/Script/Basic/MonoSinglaten.cs
+++ Project-M/Assets/Script/Basic/MonoSinglaten.cs
@@ -3,24 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MonoSiglaten<T> : MonoBehaviour where T : MonoBehaviour{
-    private static T _instance = null;
 
-    public static T instance {
-        get{
-            if ((object)MonoSiglaten<T>._instance == null) {
-                MonoSiglaten<T>._instance = new T();
-            }
-            else {
-                if (instance != this) {
-                    
-                }
-            }
-        }
-    }
-
-    private void Awake() {
-        if (instance == null) {
-            
-        }
-    }
 }
