75c981af90534b03416c1603cb812156af72ee81
H0nok4 <45478238+chenweidada@users.noreply.github.com>
2021-12-26 22:43:42 +0800

    assets:基础的一些尝试

--- Project-M/Assets/Script/MVC/Controller/PlayerController.cs
+++ Project-M/Assets/Script/MVC/Controller/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]GameObject _character;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +12,29 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (isCharacterFlip(_character.transform.position)) {
+            _character.transform.localScale = new Vector3(1,1,1);
+        } else {
+            _character.transform.localScale = new Vector3(-1,1,1);
+        }
+
+
+    }
+
+    /// <summary>
+    /// 需要判断玩家鼠标位置，角色应该面向鼠标那一侧
+    /// </summary>
+    public bool isCharacterFlip(Vector3 _characterPos) {
+        var midPos = new Vector3(Screen.width / 2,Screen.height / 2);
+        var mousePos = Input.mousePosition;
+        var targetPos = mousePos - midPos;
+
+        if (targetPos.x > _characterPos.x) {
+            return true;
+        }
+
+        return false;
     }
 }
