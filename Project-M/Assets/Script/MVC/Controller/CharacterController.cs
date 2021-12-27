fde06c532a8817b7e20cdcd65a895f9fe904e5ac
H0nok4 <45478238+chenweidada@users.noreply.github.com>
2021-12-27 23:48:54 +0800

    assets:角色数据

--- Project-M/Assets/Script/MVC/Controller/CharacterController.cs
+++ Project-M/Assets/Script/MVC/Controller/CharacterController.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    private CharacterModel _model;
+
+    public CharacterController() {
+        _model = new CharacterModel();
+    }
+
 
 }
---
+++ Project-M/Assets/Script/MVC/Model/CharacterModel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterModel
+{
+    public int Hp;
+    public int Def;
+    public int Att;
+   
+}
