269e3444d013c4aa20ee1642698c6709a84c48af
H0nok4 <45478238+chenweidada@users.noreply.github.com>
2022-01-01 19:44:11 +0800

    assets:注释

--- Project-M/Assets/Script/MVC/Controller/CameraController.cs
+++ Project-M/Assets/Script/MVC/Controller/CameraController.cs
@@ -8,6 +8,7 @@ public class CameraController : MonoBehaviour
     public GameObject CharacterObject;//临时测试用
 
     private void FixedUpdate() {
+        //只是测试用，后续需要抽象成方法，并且修改各个参数
         var mouseWorldPosition = GetPlayerMouseWorldPosition();
         Debug.Log($"mouseWorldPosition = {mouseWorldPosition}");
         var characterPos = CharacterObject.transform.position;
@@ -16,6 +17,8 @@ public class CameraController : MonoBehaviour
         float targetCameraPosY = ((mouseWorldPosition)).y;
         targetCameraPosY = Mathf.Clamp(targetCameraPosY,characterPos.y - 3,characterPos.y + 3);
         var cameraPos = Camera.main.transform.position;
+
+        //因为当鼠标靠近玩家角色时，玩家的目标可能就在眼前，不应该拉长镜头，故只在鼠标位置超出玩家角色位置过多的时候才拉长镜头
         if (targetCameraPosX >= characterPos.x + 3 || targetCameraPosX <= characterPos.x - 3 || targetCameraPosY >= characterPos.y + 3 || targetCameraPosY <= characterPos.y - 3) {
             Camera.main.transform.position = new Vector3(Mathf.Lerp(cameraPos.x,targetCameraPosX,3 * Time.deltaTime),Mathf.Lerp(cameraPos.y,targetCameraPosY,3 * Time.deltaTime),-10);
         } else {
