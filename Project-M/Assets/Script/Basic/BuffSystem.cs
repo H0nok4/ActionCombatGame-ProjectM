b8d4a32bec7eb58ab3eb3e40c5b708e6e962327d
H0nok4 <45478238+chenweidada@users.noreply.github.com>
2022-01-30 20:00:58 +0800

    assets:整理文件，Buff系统准备

---
+++ Project-M/Assets/Script/Basic/BuffSystem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffSystem : Singleton<BuffSystem>
+{
+    public void Init() {
+        //TODO:≥ı ºªØBuffSystem
+    }
+}
